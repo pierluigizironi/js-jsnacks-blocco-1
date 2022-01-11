02665b84d0969a06053b57bfb193c93f500fb913
pierluigizironi <zironipierluigi@gmail.com>
2022-01-11 17:41:51 +0100

    Add jsnack2

--- js/script2.js
+++ js/script2.js
@@ -1,4 +1,12 @@
-const firstWord = prompt('Inserisci la prima parola');
+const firstWord = prompt('Inserisci la prima parola').trim();
 console.log('Prima Parola:', firstWord);
-const secondWord = prompt('Inserisci la seconda parola');
+const secondWord = prompt('Inserisci la seconda parola').trim();
 console.log('Seconda Parola:', secondWord);
+
+if (firstWord.length > secondWord.length) {
+  console.log(`'ordine è il seguente: ${secondWord} e ${firstWord} `)
+} else if (firstWord.length < secondWord.length) {
+    console.log(`L'ordine è il seguente: ${firstWord} e ${secondWord} `)
+} else {
+    console.log('Hanno pari lunghezza')
+}
