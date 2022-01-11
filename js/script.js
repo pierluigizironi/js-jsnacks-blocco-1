39668f070728b09bf4244a1838719a55deea421b
pierluigizironi <zironipierluigi@gmail.com>
2022-01-11 17:17:38 +0100

    add first jsnack

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 console.log('JS OK')
+
+const firstNumber = parseInt(prompt('Inserisci il primo numero'));
+console.log('Primo Numero:', firstNumber);
+const secondNumber = parseInt(prompt('Inserisci il secondo numero'));
+console.log('Secondo Numero:', secondNumber);
+
+if (secondNumber > firstNumber) {
+    resultPage.innerHTML = `${secondNumber}`
+    console.log(secondNumber)
+} else if (firstNumber > secondNumber) {
+    console.log(firstNumber)
+    resultPage.innerHTML = `${firstNumber}`
+} else {
+    console.log('Ritenta')
+    resultPage.innerHTML = `Ritenta`
+}
