b198fb477570f959298d652766c1b33725cf3619
pierluigizironi <zironipierluigi@gmail.com>
2022-01-11 17:22:37 +0100

    add first two prompts

---
+++ js/script2.js
@@ -0,0 +1,4 @@
+const firstWord = prompt('Inserisci la prima parola');
+console.log('Prima Parola:', firstWord);
+const secondWord = prompt('Inserisci la seconda parola');
+console.log('Seconda Parola:', secondWord);
